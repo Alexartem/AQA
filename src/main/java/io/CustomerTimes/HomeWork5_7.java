de1b4adc2f57be8a60eb51e40fa8178f6092536e
oleksandrartemenko <alexartem27@gmail.com>
2019-03-19 23:56:06 +0200

    plus 5_8

---
+++ src/main/java/io/CustomerTimes/HomeWork5_7.java
@@ -0,0 +1,30 @@
+package io.CustomerTimes;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HomeWork5_7 {
+
+    public static void main(String[] args) {
+
+
+
+        HashMap<String, String> myHashMap = new HashMap<String, String>();
+        myHashMap.put("watermelon", "berry");
+        myHashMap.put("banana", "fruit");
+        myHashMap.put("cherry", "berry");
+        myHashMap.put("pineapple", "fruit");
+        myHashMap.put("melon", "vegetable");
+        myHashMap.put("cranberry", "berry");
+        myHashMap.put("apple", "fruit");
+        myHashMap.put("potato", "vegetable");
+        myHashMap.put("carrot", "vegetable");
+        myHashMap.put("iris", "flower");
+
+
+        for(HashMap.Entry<String, String> entry: myHashMap.entrySet()){
+
+            System.out.println(entry.getKey() + " - " + entry.getValue());
+        }
+    }
+}
---
+++ src/main/java/io/CustomerTimes/HomeWork5_8.java
@@ -0,0 +1,38 @@
+package io.CustomerTimes;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HomeWork5_8 {
+    public static void main(String[] args) {
+
+
+        HashMap<String, String> myHashMap = new HashMap<String, String>();
+
+        myHashMap.put("watermelon", "berry");
+        myHashMap.put("banana", "fruit");
+        myHashMap.put("cherry", "berry");
+        myHashMap.put("pineapple", "fruit");
+        myHashMap.put("melon", "vegetable");
+        myHashMap.put("cranberry", "berry");
+        myHashMap.put("apple", "fruit");
+        myHashMap.put("potato", "vegetable");
+        myHashMap.put("carrot", "vegetable");
+        myHashMap.put("iris", "flower");
+
+
+        for(HashMap.Entry<String, String> entry: myHashMap.entrySet()) {
+            if (entry.getValue() == "vegetable")
+                System.out.println(entry.getKey() + " - " + entry.getValue());
+
+        }
+
+
+
+
+
+
+
+
+    }
+}
