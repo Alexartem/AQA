4a735fb087194eb428409fbf3aee47a9633e15b0
oleksandrartemenko <alexartem27@gmail.com>
2019-03-19 20:25:19 +0200

    plus 5_6

---
+++ src/main/java/io/CustomerTimes/HomeWork5_6.java
@@ -0,0 +1,49 @@
+package io.CustomerTimes;
+
+import java.util.ArrayList;
+
+
+
+public class HomeWork5_6 {
+    public static void main(String[] args) {
+
+
+        ArrayList<String> text = new ArrayList<>();
+        ArrayList<String> max = new ArrayList<>();
+
+        text.add("Java");
+        text.add("Python2");
+        text.add("JS");
+        text.add("Ruby");
+        text.add("C#");
+        text.add("Java");
+        text.add("Python3");
+        text.add("JS");
+        text.add("Ruby");
+        text.add("C#");
+
+//        for (String x : text) {
+//
+//            System.out.println(x);
+//
+//        }
+
+        int largestString = text.get(0).length();
+        int index= 0;
+
+        for(int i = 0; i < text.size(); i++) {
+            if(text.get(i).length() > largestString) {
+                largestString = text.get(i).length();
+                index = i;
+                max.add(text.get(largestString));
+
+
+            }
+        }
+        for (int i = 0; i<text.size(); i++)
+            if (text.get(i).length() == largestString)
+                System.out.println (text.get (i) + " index "+i);
+
+    }
+
+}
