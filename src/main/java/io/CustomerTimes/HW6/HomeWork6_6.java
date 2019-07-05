918021327634ec4df6ae8ab1ad7afe9d07e01afd
oleksandrartemenko <alexartem27@gmail.com>
2019-07-05 18:17:58 +0300

    full HW6

--- src/main/java/io/CustomerTimes/HW6/HomeWork6_6.java
+++ src/main/java/io/CustomerTimes/HW6/HomeWork6_6.java
@@ -1,4 +1,35 @@
 package io.CustomerTimes.HW6;
 
+
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+
 public class HomeWork6_6 {
+
+    public static void main(String[] args) {
+        readData();
+    }
+
+    public static void readData(){
+        List<Integer> myList = new ArrayList<Integer>();
+
+        try{
+            System.out.println("Enter numbers");
+            int a ;
+            do {
+                Scanner num = new Scanner(System.in);
+                a = num.nextInt();
+
+                myList.add(a);
+
+            } while (true);
+
+        }catch (InputMismatchException e) {
+            System.out.println(myList);
+        }
+
+    }
 }
